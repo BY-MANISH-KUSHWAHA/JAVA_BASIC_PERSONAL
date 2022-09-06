abfaabc7d94157ca87a1d2fc9feed993b8db2300
BY-MANISH-KUSHWAHA-MAC <manish.kushwaha@blueyonder.com>
2022-09-06 08:41:28 +0530

    MAC_Upadte - 01

--- Variable.java
+++ Variable.java
@@ -1,6 +1,6 @@
 public class Variable {
     public static void main(String[] args) {
         // Ctrl+Shift+K for commit+Push
-        System.out.println("Hello");
+        System.out.println("Hello MAC");
     }
 }
